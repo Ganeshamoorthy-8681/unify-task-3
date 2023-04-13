7ed82da559ff012f6407cc26f2e007a00fe59a16
Ganeshamoorthy-8681 <127597753+Ganeshamoorthy-8681@users.noreply.github.com>
2023-04-13 18:18:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+let menu_btn  =  document.getElementsByClassName('navbar__menu-icon')[0];
+let  navbar__mobile  = document.getElementsByClassName('navbar__mobile-view')[0];
+let close_btn =  document.getElementsByClassName('navbar__mobile-view--close-icon')[0];
+let overlay =   document.getElementById("overlay") ; 
+let match  =  window.matchMedia('(max-width:700px)');
+
+
+
+function close_mobile_nav(){
+      navbar__mobile.classList.remove('navbar__mobile-view--expanded');
+      navbar__mobile.classList.add('navbar__mobile-view--compressed');
+      overlay.style.display = "none";
+}
+
+function open_mobile_nav(){
+      navbar__mobile.classList.add('navbar__mobile-view--expanded');
+      navbar__mobile.classList.remove('navbar__mobile-view--compressed');
+      overlay.style.display = "block";
+}
+
+
+overlay.addEventListener('click',()=>{
+    close_mobile_nav();
+})  
+
+
+menu_btn.addEventListener("click",()=>{
+    open_mobile_nav();
+})
+
+
+close_btn.addEventListener('click',()=>{
+    close_mobile_nav();
+})  
+
+
+match.addEventListener("change",()=>{
+    if (! match.matches)
+        close_mobile_nav()
+})
+
+
+
+
+
+
